35a430dc5481ae425cc5bff07a4a69fb97a81b51
P34RL <undeadgamer279@gmail.com>
2024-01-21 14:27:03 -0500

    added coments for readability

--- src/ip.rs
+++ src/ip.rs
@@ -8,11 +8,20 @@ use std::{
 
 //                     Wont Change        Might Change            Will Change (should be same count)     Wont Change                 Number Will Be 10-15  Wont Change               Wont Change
 const HEADERS: &str = "HTTP/1.1 200 OK\r\nServer: nginx/1.25.1\r\nDate: Sun, 21 Jan 2024 03:15:34 GMT\r\nContent-Type: text/plain\r\nContent-Length: 14\r\nConnection: keep-alive\r\nVary: Origin\r\n\r\n";
-
 const IP_START: usize = HEADERS.len();
-const IP_MAX_LENGTH: usize = 15;
 
+//                           225.225.225.225
+const IP_MAX_LENGTH: usize = 15;
 const BUFFER_LENGTH: usize = IP_START + IP_MAX_LENGTH;
+
+macro_rules! continue_if_err {
+    ($a:expr) => {
+        match $a {
+            Ok(v) => v,
+            _ => continue,
+        }
+    };
+}
 
 fn get_public_ip() -> Result<IpAddr> {
     // Make a TCP connection to the ipify.org API
@@ -45,10 +54,7 @@ pub fn spawn_get_ip_task(tx: mpsc::Sender<IpAddr>) -> thread::JoinHandle<()> {
         thread::sleep(INTERVAL);
 
         // Call the function
-        let new_ip = match get_public_ip() {
-            Ok(v) => v,
-            Err(_) => continue,
-        };
+        let new_ip = continue_if_err!(get_public_ip());
 
         // Check if the ip address has changed
         if new_ip != current_ip {
